48dd627157104e2c012dedf75830ee037741e5eb
Guilhaume de Gineste <guilhaume.degineste@gmail.com>
2022-05-26 15:49:47 +0200

    ajout couleur

--- L3java/src/plateau/CaseCamp.java
+++ L3java/src/plateau/CaseCamp.java
@@ -6,12 +6,10 @@ import joueur.Joueur;
 
 public class CaseCamp extends Case {
 
-	private Joueur joueur;
 	
-	public CaseCamp(int x, int y, Joueur joueur, Color couleur) {
+	public CaseCamp(int x, int y, Color couleur) {
 		super(x, y, couleur);
 		// TODO Auto-generated constructor stub
-		this.joueur = joueur;
 	}
 
 	@Override
